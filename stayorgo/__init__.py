30a2b9c5739a737731c01705b05ec5d7e4d34d58
Cameron Rodda <cameron.rodda@synchrotron.org.au>
2016-12-18 07:54:53 +1100

    fixed cache declarations

--- stayorgo/__init__.py
+++ stayorgo/__init__.py
@@ -9,13 +9,12 @@ app = Flask(__name__)
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 StaticCDN(static_endpoint='/static')
 
-cache = Cache(config={'CACHE_TYPE': 'filesystem',
-                      'CACHE_DEFAULT_TIMEOUT': '300',
-                      'CACHE_DIR': BASE_DIR + '/static/cache'})
+cache = Cache(app,config={'CACHE_TYPE': 'filesystem',
+                      'CACHE_DEFAULT_TIMEOUT': 300,
+                      'CACHE_DIR': BASE_DIR + '/static/cache',
+                      'CACHE_THRESHOLD': 5})
 
 Bootstrap(app)
-
-# Bootstrap.StaticCDN(BASE_DIR+'/static/')
 
 import stayorgo.config
 import stayorgo.views
