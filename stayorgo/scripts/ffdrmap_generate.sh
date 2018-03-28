e436c202cc173db6749efd433e8684b5026b3f5a
Nomad Cam <nomad-cam@mailinator.com>
2018-03-28 21:19:14 +1100

    added some echo statements to follow steps

--- stayorgo/scripts/ffdrmap_generate.sh
+++ stayorgo/scripts/ffdrmap_generate.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
+echo "Starting Script: ffdrmap_generate.sh"
 IMG_PATH=/home/rohshamboyha/stay-or-go/stayorgo/static/img/auto
 #wget -q http://www.bom.gov.au/fwo/IDV65406.png > $IMG_PATH/IDV65406.png
 curl http://www.bom.gov.au/fwo/IDV65406.png > $IMG_PATH/IDV65406.png
+echo "cURL Complete..."
 convert $IMG_PATH/IDV65406.png -stroke black -draw "line 37,27 37,1" -draw "line 78,27, 78,1" -draw "line 158,27 158,1" -draw "line 236,27 236,1" -draw "line 313,27 313,1" -fuzz 13% -fill green -opaque '#41aea4' -opaque '#0d84f4' -fuzz 8.3% -fill '#00adee' -opaque '#7abe47' -opaque '#b5d529' -fuzz 10% -fill '#00adee' -opaque '#6ac461' -fuzz 10.7% -fill '#fef200' -opaque '#f7df1e' -fuzz 10% -fill '#fe9929' -opaque '#feaa1e' -fuzz 7% -opaque '#fe8921' -fuzz 12.5% -fill '#ef2c21' -opaque '#fe4e28' -fuzz 13% -fill grey40 -opaque '#f02166' -opaque '#940b93' -fill '#7ac241' -opaque green $IMG_PATH/IDV65406_tmp.png
-
+echo "Colourise Complete..."
 convert	$IMG_PATH/IDV65406_tmp.png $IMG_PATH/state_legend_overlay.png -composite $IMG_PATH/IDV65406_mod.png
+echo "Image Composition Complete..."
